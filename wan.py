7c3e1bfdc954feb3ab78247ea23fc92c4dced3ff
wn <714970851@qq.com>
2020-04-06 22:03:19 +0800

    das

--- wan.py
+++ wan.py
@@ -1,11 +1,10 @@
-das
-ww12dsa
-d3dsa
-dsa
-das
-dfas
-da
-d
-ad
-as
-:
+import flask
+app = flask.Flask(__name__)
+@app.route("/")
+def wang():
+    return "wad"
+@app.route("/wn")
+def ning():
+    return "wadasdsadsadas"
+if __name__ == '__main__':
+    app.run()
--- wang.py
+++ wang.py
@@ -1,4 +1,26 @@
-王宁  你好
-你好
-你好！！！！
+import fire
+import datetime
 
+
+def cal_days(date_str1, date_str2):
+    '''计算两个日期之间的天数'''
+
+    date_str1 = str(date_str1)
+    date_str2 = str(date_str2)
+    d1 = datetime.datetime.strptime(date_str1, '%Y%m%d')
+    d2 = datetime.datetime.strptime(date_str2, '%Y%m%d')
+
+    return delta.days
+
+
+def days2today(date_str):
+    '''计算某天距离今天的天数'''
+
+    date_str = str(date_str)
+    d = datetime.datetime.strptime(date_str, '%Y%m%d')
+    delta = datetime.datetime.now() - d
+    return delta.days
+
+
+if __name__ == '__main__':
+    fire.Fire(cal_days)
